7a0a7ad542ed8f1ce747b5c9f00c968ddc5b93c6
Leo <leocasti2@gmail.com>
2025-11-13 06:28:24 -0500

    refactor: elimina dependencias innecesarias del setup

--- setup.py
+++ setup.py
@@ -11,8 +11,6 @@ setup(
         "click==8.3.0",
         "gitingest==0.3.1",
         "google-api-python-client==2.187.0",
-        "selenium==4.38.0",
-        "undetected-chromedriver==3.5.5",
     ],
     keywords="cli google-ai-studio project-context",
     include_package_data=True,
